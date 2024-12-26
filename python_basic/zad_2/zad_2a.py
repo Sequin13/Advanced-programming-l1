eb846ccfafb59980f14ee310ed9984d9b365f564
Sequin13 <116645119+Sequin13@users.noreply.github.com>
2024-12-26 14:46:42 +0100

    Reformating code style into pep8

--- python_basic/zad_2/zad_2a.py
+++ python_basic/zad_2/zad_2a.py
@@ -3,5 +3,5 @@ def show_names(list_of_names):
         print(name)
 
 
-names = ['Jakub', 'Remigiusz', 'Adam', 'Maciej', 'Kamil']
+names = ["Jakub", "Remigiusz", "Adam", "Maciej", "Kamil"]
 show_names(names)
